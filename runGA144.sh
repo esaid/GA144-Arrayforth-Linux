b822ca237b89685900fc958a942c0ea7373f1a7c
Ga144 <emmanuel.said@gmail.com>
2022-08-13 19:02:06 +0200

    Add files via upload

---
+++ runGA144.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Auteur : Emmanuel SAID
+# Date : Aout 2022
+# Version 1.0.0 : configuration port USB serie ttyUSB0 lancement Arrayforth
+#
+
+# Affiche un texte à l'écran :
+echo "Configuration ttyUSB0"
+echo "ttyUSB0 droit acces "
+sudo chmod 666 /dev/ttyUSB0
+echo " vitesse ttyUSB0 921600"
+sudo stty -F /dev/ttyUSB0 921600 -parenb cs8 -cstopb -crtscts raw -echo
+echo " com --> ttyUSB0  "
+stty -F /dev/ttyUSB0 | grep speed
+cd ~/.wine/dosdevices/
+echo " ttyUSB0 est attache au COM"
+ls -l | grep ttyUSB0
+echo " lancement programme Arrayforth "
+cd ~/.wine/drive_c/GreenArrays/EVB002/
+wine Okad2-42c-pd.exe
---
+++ ttyUSB0_configuration.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Auteur : Emmanuel SAID
+# Date : Aout 2022
+# Version 1.0.0 : configuration port USB serie ttyUSB0 lancement Arrayforth
+#
+
+# Affiche un texte à l'écran :
+echo "Configuration ttyUSB0"
+echo "ttyUSB0 droit acces "
+groups
+sudo usermod -a -G dialout $USERNAME
+ls -l /dev/ttyUSB0
+echo " redemarrer la session "
+
